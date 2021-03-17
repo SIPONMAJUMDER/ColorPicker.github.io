5dc4f7ef4d5d740e95d5996507c0416e7b1a4785
SiponMajumder <siponmajumder237@gmail.com>
2021-03-17 17:21:49 +0530

    My Own Color Picker
    This Website Have Gradient and Solid colors

---
+++ js/Color.js
@@ -0,0 +1,19 @@
+var type = new Typed("#type", {
+    strings: [
+        '',
+        'Design By Sipon Majumder',
+        'Pick Your Color',
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 1000,
+      loop: true,
+  });
+
+
+  // document.getElementById('copybtn').addEventListener('click',()=>{
+  //   document.getElementById('copyarea').select();
+  //   document.execCommand('copy');
+  // })
+  
+ 
